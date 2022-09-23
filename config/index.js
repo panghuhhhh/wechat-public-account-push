44edb59442646c9903be2252dd48991be8ca1a40
panghuhhhh <114207580+panghuhhhh@users.noreply.github.com>
2022-09-23 12:52:38 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "",
+  APP_ID: "wx4d1e6614f704b96a",
   
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "",
+  APP_SECRET: "905ab1f19b55219b86f596fa2b977e72",
   
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -95,8 +95,8 @@ export const USER_CONFIG = {
       ]
     },
     {
-      name: "老婆1",
-      id: "",
+      name: "oC5EX6AJfTkNnWT7RA69CemRtpp4",
+      id: "5Yw2dcSIH22xVyNf7cx_BCJeKzf6CmvT2wefW6OMoBk",
       useTemplateId: "",
       province: "",
       city: "",
